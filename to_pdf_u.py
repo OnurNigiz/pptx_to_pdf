a8b01068521fc21b02e7475b62d9ec1b467de3b2
Onur Nigiz <75140593+OnurNigiz@users.noreply.github.com>
2023-02-28 01:02:04 +0300

    initial commit

---
+++ to_pdf_u.py
@@ -0,0 +1,11 @@
+import os
+
+input_folder = os.path.join(os.path.expanduser('~'), 'Desktop', 'willConvert')
+output_folder = os.path.join(os.path.expanduser('~'), 'Desktop', 'Converted')
+
+for root, dirs, files in os.walk(input_folder):
+    for file in files:
+        if file.endswith('.pptx'):
+            input_path = os.path.join(root, file)
+            output_path = os.path.join(output_folder, os.path.splitext(file)[0] + '.pdf')
+            os.system('libreoffice --headless --convert-to pdf "{}" --outdir "{}"'.format(input_path, output_folder))
---
+++ to_pdf_w.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+input_folder = os.path.join(os.path.expanduser('~'), 'Desktop', 'kurabiyem')
+output_folder = os.path.join(os.path.expanduser('~'), 'Desktop', 'Converted')
+
+for root, dirs, files in os.walk(input_folder):
+    for file in files:
+        if file.endswith('.pptx'):
+            input_path = os.path.join(root, file)
+            output_path = os.path.join(output_folder, os.path.splitext(file)[0] + '.pdf')
+            process = subprocess.Popen(['C:\\Program Files\\Microsoft Office\\root\\Office16\\POWERPNT.EXE', '/pt', input_path, output_path])
+            stdout, stderr = process.communicate()
